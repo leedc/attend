84a6d78c5017c3fb4ef263784fa9733942d7617d
leedc <lidc.Dodge@gmail.com>
2015-05-22 18:14:09 +0800

    忘了

--- application/controllers/Search.php
+++ application/controllers/Search.php
@@ -10,8 +10,71 @@ class SearchController extends Yaf_Controller_Abstract
     {
 
     }
-    public function  searchAction()
+    public function  majorlistAction()
     {
+        $userid = Yaf_Session::getInstance()->get("user");
+        $dbh = Yaf_Registry::get('_db');
+        $rs = $dbh->query("select * from teacher where tid='{$userid}'");
+        $user = $rs->fetch();
+        $this -> getView() -> assign("user",$user);
 
+
+        return true;
     }
+    public function  majorbrowseAction()
+    {
+        $userid = Yaf_Session::getInstance()->get("user");
+        $dbh = Yaf_Registry::get('_db');
+        $rs = $dbh->query("select * from teacher where tid='{$userid}'");
+        $user = $rs->fetch();
+        $this -> getView() -> assign("user",$user);
+
+
+        return true;
+    }
+    public function  gradelistAction()
+    {
+        $userid = Yaf_Session::getInstance()->get("user");
+        $dbh = Yaf_Registry::get('_db');
+        $rs = $dbh->query("select * from teacher where tid='{$userid}'");
+        $user = $rs->fetch();
+        $this -> getView() -> assign("user",$user);
+
+
+        return true;
+    }
+    public function  gradebrowseAction()
+    {
+        $userid = Yaf_Session::getInstance()->get("user");
+        $dbh = Yaf_Registry::get('_db');
+        $rs = $dbh->query("select * from teacher where tid='{$userid}'");
+        $user = $rs->fetch();
+        $this -> getView() -> assign("user",$user);
+
+
+        return true;
+    }
+    public function  classlistAction()
+    {
+        $userid = Yaf_Session::getInstance()->get("user");
+        $dbh = Yaf_Registry::get('_db');
+        $rs = $dbh->query("select * from teacher where tid='{$userid}'");
+        $user = $rs->fetch();
+        $this -> getView() -> assign("user",$user);
+
+
+        return true;
+    }
+    public function  classbrowseAction()
+    {
+        $userid = Yaf_Session::getInstance()->get("user");
+        $dbh = Yaf_Registry::get('_db');
+        $rs = $dbh->query("select * from teacher where tid='{$userid}'");
+        $user = $rs->fetch();
+        $this -> getView() -> assign("user",$user);
+
+
+        return true;
+    }
+
 }
